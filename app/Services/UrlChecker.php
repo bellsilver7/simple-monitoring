e101460c4ee46806bd8fe04312f5dfbe82ec7574
bellsilver7 <bellsilver7@gmail.com>
2022-10-07 08:01:18 +0900

    ✨ Worked on the notification when a URL check fails.

--- app/Services/UrlChecker.php
+++ app/Services/UrlChecker.php
@@ -2,7 +2,10 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use App\Notifications\UrlFailedNotification;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Notification;
 
 class UrlChecker
 {
@@ -13,6 +16,17 @@ class UrlChecker
         $totalTime = microtime(true) - $startTime;
         logger($totalTime);
 
+        if ($response->failed()) {
+            $this->notifyOnError($url);
+            return false;
+        }
+
         return $response->ok();
+    }
+
+    private function notifyOnError(string $url)
+    {
+        $user = User::find(1);
+        Notification::send($user, new UrlFailedNotification($url));
     }
 }
