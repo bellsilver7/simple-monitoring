57246565b49ad87936cc9a7037887a0630cf3d44
bellsilver7 <bellsilver7@gmail.com>
2022-10-15 19:14:31 +0900

    🎨breadcrumbs inertia link

--- resources/js/Components/Breadcrumbs.vue
+++ resources/js/Components/Breadcrumbs.vue
@@ -1,20 +1,26 @@
 <script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+
 defineProps({
-    items: Array
-})
+  items: Array,
+});
 </script>
 <template>
-    <ol class="flex text-gray-700 bg-gray-100 rounded py-2 px-2">
-        <template v-for="(item, index) in items">
-            <li class="px-2" :class="{'text-indigo-600': item.disabled}">
-                <template v-if="item.href !== ''">
-                    <a :href="item.href" class="hover:underline">{{ item.text }}</a>
-                </template>
-                <template v-else>
-                    {{ item.text }}
-                </template>
-            </li>
-            <li v-if="index < items.length - 1" class="text-gray-500 select-none">&rsaquo;</li>
+  <ol class="flex text-gray-700 bg-gray-100 rounded py-2 px-2">
+    <template v-for="(item, index) in items">
+      <li class="px-2" :class="{ 'text-indigo-600': item.disabled }">
+        <template v-if="item.href !== ''">
+          <Link :href="item.href" class="hover:underline">
+            {{ item.text }}
+          </Link>
         </template>
-    </ol>
+        <template v-else>
+          {{ item.text }}
+        </template>
+      </li>
+      <li v-if="index < items.length - 1" class="text-gray-500 select-none">
+        &rsaquo;
+      </li>
+    </template>
+  </ol>
 </template>
