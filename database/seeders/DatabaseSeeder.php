985e8b12910013720bfdb8d35e86047b9d80cb0b
bellsilver7 <bellsilver7@gmail.com>
2022-10-21 07:21:04 +0900

    DatabaseSeeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,22 +8,30 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    // \App\Models\User::factory(10)->create();
 
-         \App\Models\User::factory()->create([
-             'name' => 'Test User',
-             'email' => 'test@example.com',
-         ]);
+    \App\Models\User::factory()->create([
+      'name' => 'Test User',
+      'email' => 'test@example.com',
+    ]);
 
-        Url::create(['url' => 'https://www.amitavroy.com']);
-        Url::create(['url' => 'https://www.amitavroy.com/test']);
-        Url::create(['url' => 'https://focalworks.in']);
+    Url::create(['url' => 'https://www.amitavroy.com']);
+    $url = Url::create(['url' => 'https://www.amitavroy.com/test']);
+
+    for ($i = 0; $i < 5; $i++) {
+      $time = now()->addMinutes(5 * $i);
+      $url->failures()->create([
+        'updated_at' => $time,
+        'created_at' => $time,
+      ]);
     }
+    Url::create(['url' => 'https://focalworks.in']);
+  }
 }
